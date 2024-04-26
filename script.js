f96173d3b08d9b17af2b3cca83ae30b064f0d377
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2024-04-26 10:54:10 +0330

    modify script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let textarea=document.querySelector('textarea')
 let textareaWrapper=document.querySelector('.container')
-let value
-let regEX=/^\w$/
 
-let emailRegEX=/^(www\.)?\w+([\.-]?\w+)*@\w+([\.-]?\w+)*\.\w{2,4}$/
 
-let array
+
+
+
 
  textarea.addEventListener('keyup',event=>textareaWrapper.innerHTML=event.target.value)
  
- document.querySelector('.btn').addEventListener('click',compileFunction)
+ //document.querySelector('.btn').addEventListener('click',compileFunction)
 
 
 
@@ -31,136 +30,146 @@ let keyWords=[{name:'bool', token:'T_Bool'},{name:'break',token:'T_Break'},{name
 {name:'true',token:'T_True'}
 ]
 
-let state_0={
+let states={
+
+
+
+ state_0:{
     stateNumber:0,
-    whiteSpace:{nextState:state_0, tokens:['T_Whitespace']},
-    multiplication:{nextState:state_0, tokens:['T_AOp_ML']},
-    division:{nextState:state_18,tokens:null},
-    reminder:{nextState:state_0,tokens:['T_AOp_RM']},
-    parenthesisOpen:{nextState:state_0,tokens:['T_LP']},
-    parenthesisClose:{nextState:state_0,tokens:['T_RP']},
-    braceOpen:{nextState:state_0,tokens:['T_LC']},
-    braceClose:{nextState:state_0,tokens:['T_RC']},
-    bracketOpen:{nextState:state_0,tokens:['T_LB']},
-    bracketClose:{nextState:state_0,tokens:['T_RB']},
-    comma:{nextState:state_0,tokens:['T_Comma']},
-    semicolon:{nextState:state_0,tokens:['T_Semicolon']},
-    plus_Minus:{nextState:state_1,tokens:null},
-    number1_9:{nextState:state_3,tokens:null},
-    number0:{nextState:state_2,tokens:null},
-    a_ZA_Z_:{nextState:state_6,tokens:null},
-    less:{nextState:state_7,tokens:null},
-    greater:{nextState:state_8,tokens:null},
-    equal:{nextState:state_9,tokens:null},
-    not:{nextState:state_10,tokens:null},
-    and:{nextState:state_11,tokens:null},
-    or:{nextState:state_12,tokens:null},
-    quotation:{nextState:state_13,tokens:null},
-    doubleQuotation:{nextState:state_16,tokens:null},
-}
+    whiteSpace:{nextState:0, tokens:['T_Whitespace']},
+    multiplication:{nextState:0, tokens:['T_AOp_ML']},
+    division:{nextState:18,tokens:null},
+    reminder:{nextState:0,tokens:['T_AOp_RM']},
+    parenthesisOpen:{nextState:0,tokens:['T_LP']},
+    parenthesisClose:{nextState:0,tokens:['T_RP']},
+    braceOpen:{nextState:0,tokens:['T_LC']},
+    braceClose:{nextState:0,tokens:['T_RC']},
+    bracketOpen:{nextState:0,tokens:['T_LB']},
+    bracketClose:{nextState:0,tokens:['T_RB']},
+    comma:{nextState:0,tokens:['T_Comma']},
+    semicolon:{nextState:0,tokens:['T_Semicolon']},
+    plus_Minus:{nextState:1,tokens:null},
+    number1_9:{nextState:3,tokens:null},
+    number0:{nextState:2,tokens:null},
+    a_ZA_Z_:{nextState:6,tokens:null},
+    less:{nextState:7,tokens:null},
+    greater:{nextState:8,tokens:null},
+    equal:{nextState:9,tokens:null},
+    not:{nextState:10,tokens:null},
+    and:{nextState:11,tokens:null},
+    or:{nextState:12,tokens:null},
+    quotation:{nextState:13,tokens:null},
+    doubleQuotation:{nextState:16,tokens:null},
+},
 
-let state_1={
+ state_1:{
     stateNumber:1,
-    number1_9:{nextState:state_3, tokens:null}
-}
+    number1_9:{nextState:3, tokens:null}
+},
 
-let state_3={
-    plus:{nextState:state_0, tokens:['T_Decimal','T_AOp_PL']},
-    minus:{nextState:state_0, tokens:['T_Decimal','T_AOp_MN']},
-    whiteSpace:{nextState:state_5, tokens:['T_Decimal','T_Whitespace']},
-    exceptWhiteSpace_plus_minus_0_9:{nextState:state_0, tokens:['T_Decimal']},
-}
+ state_3:{
+    plus:{nextState:0, tokens:['T_Decimal','T_AOp_PL']},
+    minus:{nextState:0, tokens:['T_Decimal','T_AOp_MN']},
+    whiteSpace:{nextState:5, tokens:['T_Decimal','T_Whitespace']},
+    exceptWhiteSpace_plus_minus_0_9:{nextState:0, tokens:['T_Decimal']},
+},
 
-let state_5={
-    whiteSpace:{nextState:state_5, tokens:['T_Whitespace']},
-    plus:{nextState:state_0, tokens:['T_AOp_PL']},
-    minus:{nextState:state_0, tokens:['T_AOp_MN']},
-    exceptWhiteSpace_plus_minus:{nextState:state_0, tokens:null}
-}
+ state_5:{
+    whiteSpace:{nextState:5, tokens:['T_Whitespace']},
+    plus:{nextState:0, tokens:['T_AOp_PL']},
+    minus:{nextState:0, tokens:['T_AOp_MN']},
+    exceptWhiteSpace_plus_minus:{nextState:0, tokens:null}
+},
 
-let state_2={
-    x:{nextState:state_4,tokens:null},
-    whiteSpace:{nextState:state_5, tokens:['T_Decimal','T_Whitespace']},
-    plus:{nextState:state_0, tokens:['T_Decimal','T_AOp_PL']},
-    minus:{nextState:state_0, tokens:['T_Decimal','T_AOp_MN']},
-    exceptWhiteSpace_plus_minus:{nextState:state_0, tokens:['T_Decimal']}
+ state_2:{
+    x:{nextState:4,tokens:null},
+    whiteSpace:{nextState:5, tokens:['T_Decimal','T_Whitespace']},
+    plus:{nextState:0, tokens:['T_Decimal','T_AOp_PL']},
+    minus:{nextState:0, tokens:['T_Decimal','T_AOp_MN']},
+    exceptWhiteSpace_plus_minus:{nextState:0, tokens:['T_Decimal']}
     
-}
-let state_4={
-    a_fA_F0_9:{nextstate:state_4,tokens:null},
-    whiteSpace:{nextState:state_5, tokens:['T_Hexadecimal','T_Whitespace']},
-    plus:{nextState:state_0, tokens:['T_Hexadecimal','T_AOp_PL']},
-    minus:{nextState:state_0, tokens:['T_Hexadecimal','T_AOp_MN']},
-    exceptWhiteSpace_plus_minus_a_fA_F0_9:{nextstate:state_0,tokens:['T_Hexadecimal']}
-}
-let state_6={
-    a_zA_Z_0_9_:{nextstate:state_6,tokens:null},
-    whiteSpace:{nextState:state_5,tokens:['T_Id','T_Whitespace']},
-    plus:{nextState:state_0, tokens:['T_Id','T_AOp_PL']},
-    minus:{nextState:state_0, tokens:['T_Id','T_AOp_MN']},
-    exceptWhiteSpace_plus_minus_a_ZA_Z0_9_:{nextstate:state_0,tokens:['T_Id']}
+},
+ state_4:{
+    a_fA_F0_9:{nextstate:4,tokens:null},
+    whiteSpace:{nextState:5, tokens:['T_Hexadecimal','T_Whitespace']},
+    plus:{nextState:0, tokens:['T_Hexadecimal','T_AOp_PL']},
+    minus:{nextState:0, tokens:['T_Hexadecimal','T_AOp_MN']},
+    exceptWhiteSpace_plus_minus_a_fA_F0_9:{nextstate:0,tokens:['T_Hexadecimal']}
+},
+ state_6:{
+    a_zA_Z_0_9_:{nextstate:6,tokens:null},
+    whiteSpace:{nextState:5,tokens:['T_Id','T_Whitespace']},
+    plus:{nextState:0, tokens:['T_Id','T_AOp_PL']},
+    minus:{nextState:0, tokens:['T_Id','T_AOp_MN']},
+    exceptWhiteSpace_plus_minus_a_ZA_Z0_9_:{nextstate:0,tokens:['T_Id']}
+},
+
+ state_7:{
+    equal:{nextState:0,tokens:['T_ROp_LE',]},
+    exceptEqual:{nextState:0,tokens:['T_ROp_L']}
+},
+
+ state_8:{
+    equal:{nextState:0,tokens:['T_ROp_GE']},
+    exceptEqual:{nextState:0,tokens:['T_ROp_G']}
+},
+
+ state_9:{
+    equal:{nextState:0,tokens:['T_ROp_E']},
+    exceptEqual:{nextState:0,tokens:['T_Assign']}
+},
+
+ state_10:{
+    equal:{nextState:0,tokens:['T_ROp_NE']},
+    exceptEqual:{nextState:0,tokens:['T_LOp_NOT']}
+},
+
+ state_11:{
+    and:{nextState:0,tokens:['T_LOp_AND']}
+},
+
+ state_12:{
+    or:{nextState:0,tokens:['T_LOp_OR']}
+},
+
+ state_13:{
+    backSlash:{nextState:14,tokens:null},
+    exceptQuotation:{nextState:15,tokens:null}
+},
+
+ state_14:{
+    quotation:{nextState:15,tokens:null}
+},
+
+  state_15:{
+    quotation:{nextState:0,tokens:['T_Character']}
+},
+
+ state_16:{
+    backSlash:{nextState:17,tokens:null},
+    exceptDoubleQuotation:{nextState:16,tokens:null},
+    doubleQuotation:{nextState:16,tokens:['T_String']}
+},
+
+ state_17:{
+    doubleQuotation:{nextState:16,tokens:null}
+},
+
+ state_18:{
+    slash:{nextState:19,tokens:null},
+    exceptSlasch:{nextState:0,tokens:['T_AOp_DV']}
+},
+
+ state_19:{
+    exceptNewLine:{nextState:19,tokens:null},
+    newLine:{nextState:0,tokens:['T_Comment']} 
 }
 
-let state_7={
-    equal:{nextState:state_0,tokens:['T_ROp_LE',]},
-    exceptEqual:{nextState:state_0,tokens:['T_ROp_L']}
+
 }
 
-let state_8={
-    equal:{nextState:state_0,tokens:['T_ROp_GE']},
-    exceptEqual:{nextState:state_0,tokens:['T_ROp_G']}
-}
 
-let state_9={
-    equal:{nextState:state_0,tokens:['T_ROp_E']},
-    exceptEqual:{nextState:state_0,tokens:['T_Assign']}
-}
 
-let state_10={
-    equal:{nextState:state_0,tokens:['T_ROp_NE']},
-    exceptEqual:{nextState:state_0,tokens:['T_LOp_NOT']}
-}
-
-let state_11={
-    and:{nextState:state_0,tokens:['T_LOp_AND']}
-}
-
-let state_12={
-    or:{nextState:state_0,tokens:['T_LOp_OR']}
-}
-
-let state_13={
-    backSlash:{nextState:state_14,tokens:null},
-    exceptQuotation:{nextState:state_15,tokens:null}
-}
-
-let state_14={
-    quotation:{nextState:state_15,tokens:null}
-}
-
-let state_15={
-    quotation:{nextState:state_0,tokens:['T_Character']}
-}
-
-let state_16={
-    backSlash:{nextState:state_17,tokens:null},
-    exceptDoubleQuotation:{nextState:state_16,tokens:null},
-    doubleQuotation:{nextState:state_16,tokens:['T_String']}
-}
-
-let state_17={
-    doubleQuotation:{nextState:state_16,tokens:null}
-}
-
-let state_18={
-    slash:{nextState:state_19,tokens:null},
-    exceptSlasch:{nextState:state_0,tokens:['T_AOp_DV']}
-}
-
-let state_19={
-    exceptNewLine:{nextState:state_19,tokens:null},
-    newLine:{nextState:state_0,tokens:['T_Comment']} 
-}
 
 
 
